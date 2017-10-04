3672bac1e28915e8a2a2f352571a0f25c1601a1f
Andrey Veshtard <andrey.veshtard@ctco.lv>
2017-10-04 21:04:59 +0300

    fixed urlPattern and page name for request dispatcher

--- src/main/java/lv/ctco/battleship/controller/WaitOpponentReadyServlet.java
+++ src/main/java/lv/ctco/battleship/controller/WaitOpponentReadyServlet.java
@@ -11,22 +11,25 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name = "WaitOpponentReadyServlet")
+@WebServlet(name = "WaitOpponentReadyServlet",
+        urlPatterns = "/wait-opponent-ships")
 public class WaitOpponentReadyServlet extends HttpServlet {
 
     @Inject
     private PlayerManager playerManager;
 
     @Override
+    @SuppressWarnings("MethodDoesntCallSuperMethod")
     protected void doGet(final HttpServletRequest request, final HttpServletResponse response)
             throws ServletException, IOException {
         final Player player1 = playerManager.getGame().getPlayer1();
         final Player player2 = playerManager.getGame().getPlayer2();
 
         if (player1.isReady() && player2.isReady()) {
-            response.sendRedirect(request.getContextPath() + "/replacement.jsp");
+            //noinspection NestedMethodCall
+            response.sendRedirect(request.getContextPath() + "/placement.jsp");
         } else {
-            request.getRequestDispatcher("/WEB-INF/wait-opponent-ready.jsp")
+            request.getRequestDispatcher("/WEB-INF/wait-opponent-ships.jsp")
                     .include(request, response);
         }
     }
